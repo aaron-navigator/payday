2c412c94cc79aed196f3c3deec522e9c213bc965
Aaron Mahnic <aaron@navigatormm.com>
2015-11-27 15:11:31 -0800

    Initial commit

---
+++ pd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import time
+
+today = time.strftime("%x")
+payday = '11/30/15'
+
+Drunk = 'cult classic?'
+if today == payday:
+        print 'payday in full effect'
+else:
+        print Drunk
